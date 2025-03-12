7f3feca56001ce4ef84f4205a56e2446426eac49
Ivan_Enko <110199ws@gmail.com>
2025-03-12 16:19:28 +0200

    timer is done

---
+++ src/js/1-timer.js
@@ -0,0 +1,93 @@
+import flatpickr from "flatpickr";
+import iziToast from "izitoast";
+import { refs } from "./refs.js"
+
+let userSelectedDate = null;
+let interval = null;
+
+flatpickr(refs.input,{
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectDate = selectedDates[0];
+
+    if (selectDate < new Date()) {
+
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight'
+      });
+
+      refs.btnStart.setAttribute('disabled', 'true');
+    } else {
+      userSelectedDate = selectDate;
+      refs.btnStart.removeAttribute('disabled');
+    }
+  },
+});
+
+refs.btnStart.addEventListener('click', () => {
+  start();
+});
+
+
+
+function start() {
+  const { input, days, hours, minutes, seconds } = refs;
+
+  interval = setInterval(() => {
+    const diff = userSelectedDate - Date.now();
+    refs.btnStart.setAttribute('disabled', '');
+    input.setAttribute('disabled', '');
+
+
+    if (diff < 0) {
+      input.removeAttribute('disabled');
+      refs.btnStart.removeAttribute('disabled');
+
+      stop();
+      return;
+    }
+
+    const timeComponent = convertMs(diff);
+
+    days.textContent = pad(timeComponent.days);
+    hours.textContent = pad(timeComponent.hours);
+    minutes.textContent = pad(timeComponent.minutes);
+    seconds.textContent = pad(timeComponent.seconds);
+  }, 1000)
+};
+
+function stop() {
+  clearInterval(interval);
+
+  days.textContent = '00';
+  hours.textContent = '00';
+  minutes.textContent = '00';
+  seconds.textContent = '00';
+};
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+const refs = {
+  input: document.querySelector('input[type="text"]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+  btnStart: document.querySelector('[data-start]'),
+}
+
+export { refs };
